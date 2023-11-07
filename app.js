7f208b0edb1c6790a2fd16315f72f1c7e9c28988
DahVincis <ph.leao2099@gmail.com>
2023-11-06 21:22:19 -0500

    modified:   app.js

--- app.js
+++ app.js
@@ -3,24 +3,32 @@ const fs = require('fs');
 const path = require('path');
 
 const myserver = http.createServer(function(req, res){
-    fs.readFile('lagohina.html', function(err, data) {
+    let filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './lagohina.html';
+    }
+
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml'
+    };
+
+    let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(err, data) {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
+            res.writeHead(500, {'Content-Type': 'text/html'});
+            return res.end("500 Internal Server Error");
         }
 
-        let mimeTypes = {
-            '.html': 'text/html',
-            '.js': 'text/javascript',
-            '.css': 'text/css',
-            '.json': 'application/json',
-            '.png': 'image/png',
-            '.jpg': 'image/jpg',
-            '.gif': 'image/gif',
-            '.svg': 'image/svg+xml'
-        };
-
-        res.writeHead(200, {'Content-Type': mimeTypes});
+        res.writeHead(200, {'Content-Type': contentType});
         res.write(data);
         return res.end();
     });
