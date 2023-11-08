3be498fcb91ea65cf77efee3f299a7cb963126f5
DahVincis <ph.leao2099@gmail.com>
2023-11-08 16:16:30 -0500

    modified:   HTML/admin.html 	modified:   JS/admin.js

--- JS/admin.js
+++ JS/admin.js
@@ -6,23 +6,22 @@ const path = require('path');
 // Configure Multer storage
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, '/lagoIMG')
+    cb(null, path.join(__dirname, 'lagoIMG'))
   },
   filename: function(req, file, cb) {
-    // Use the original name from the route
     cb(null, req.params.imagename + path.extname(file.originalname))
   }
 });
 
+
 const upload = multer({ storage: storage });
 
-// Create separate routes for each image
-['logo.png', 'lagoHead.png', 'sideBar.png'].forEach(imagename => {
+['logo', 'lagoHead', 'sideBar'].forEach(imagename => {
   router.post('/lagoIMG/' + imagename, upload.single(imagename), (req, res) => {
-    // After handling the upload, redirect back to the admin page
     res.redirect('/admin.html');
   });
 });
+
 
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../HTML/admin.html'));
