36750c54c1fcab5cfb323aca3332f8ba68f2faea
DahVincis <ph.leao2099@gmail.com>
2023-11-08 14:05:44 -0500

    modified:   JS/lagoinha.js 	modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Serve static files from the respective directories
 app.use(express.static('HTML'));
@@ -8,3 +9,6 @@ app.use(express.static('JS'));
 app.use(express.static('lagoIMG'));
 
 app.listen(80, () => console.log('Server started on port 80'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/HTML/lagoinha.html'));
+});
