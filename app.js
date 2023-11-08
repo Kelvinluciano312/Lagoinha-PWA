effc3283307f910ffb1cd9c5a739dca6efae491d
DahVincis <ph.leao2099@gmail.com>
2023-11-08 14:41:41 -0500

    modified:   HTML/admin.html 	modified:   HTML/lagoinha.html 	modified:   JS/admin.js 	modified:   JS/lagoinha.js 	modified:   app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const adminRoutes = require('./admin.js');
+app.use(adminRoutes);
 
-// Serve static files from the respective directories
+const lagoRoutes = require('./lagoinha.js');
+app.use(lagoRoutes);
+
 app.use(express.static('HTML'));
 app.use(express.static('CSS'));
 app.use(express.static('JS'));
 app.use(express.static('lagoIMG'));
 
 app.listen(80, () => console.log('Server started on port 80'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/lagoinha.html'));
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/HTML/admin.html'));
-});
