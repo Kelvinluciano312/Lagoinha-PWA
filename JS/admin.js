238a3dcb72e8fb3df6b89c877eb4ee90a15bcb1f
DahVincis <ph.leao2099@gmail.com>
2023-11-13 16:33:52 -0500

    modified:   JS/admin-update.js 	modified:   JS/admin.js 	modified:   JS/lagoinha.js 	modified:   app.js

--- JS/admin.js
+++ JS/admin.js
@@ -17,33 +17,19 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 ['logo', 'lagoHead', 'sideBar'].forEach(imagename => {
-  router.post('/lagoIMG/' + imagename, upload.single(imagename), (req, res, next) => {
-    // Check if file is provided
-    if (!req.file) {
-      return res.status(400).send('No file uploaded');
-    }
-
+  router.post('/lagoIMG/' + imagename, upload.single(imagename), (req, res) => {
     sharp(req.file.path)
       .toFormat('png')
       .toFile(req.file.path + '.png', (err, info) => { // Add '.png' here
-        if (err) {
-          console.error(err);
-          return res.status(500).send('Server error');
-        }
+        if (err) throw err;
         res.redirect('/admin.html');
       });
   });
 });
 
-
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../HTML/admin.html'), function(err) {
-    if (err) {
-      next(err);
-    }
-  });
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../HTML/admin.html'));
 });
-
 
 // New route for sending the latest images
 router.get('/latest-images', function(req, res) {
