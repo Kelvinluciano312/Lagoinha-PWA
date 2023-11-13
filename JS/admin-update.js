dafef6abc2be996cf52b6d34cb695d9ed991dff4
DahVincis <ph.leao2099@gmail.com>
2023-11-13 16:47:43 -0500

    modified:   JS/admin-update.js 	modified:   JS/admin.js 	modified:   JS/lagoinha-menu.js 	modified:   JS/lagoinha.js 	modified:   app.js

--- JS/admin-update.js
+++ JS/admin-update.js
@@ -1,11 +1,6 @@
 setInterval(function() {
   fetch('/latest-images')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
       ['logo', 'lagoHead', 'sideBar'].forEach(id => {
         var img = new Image();
@@ -14,8 +9,5 @@ setInterval(function() {
         };
         img.src = 'lagoIMG/' + data[id];
       });
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
     });
 }, 3000);  // Fetch every 3 second
