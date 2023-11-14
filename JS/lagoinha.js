0967d7747dd3f35c77f84ef0a326f1b22f58df52
DahVincis <ph.leao2099@gmail.com>
2023-11-13 20:05:17 -0500

    modified:   JS/lagoinha.js

--- JS/lagoinha.js
+++ JS/lagoinha.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', function(req, res, next) {
+    console.log('Received request for /');
     res.sendFile(path.join(__dirname, '../HTML/admin.html'), function(err) {
       if (err) {
         next(err);
