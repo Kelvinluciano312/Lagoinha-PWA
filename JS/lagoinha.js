3bc0947904f9ce81761ffbe53e3e5aa6af26bb75
DahVincis <ph.leao2099@gmail.com>
2023-11-13 19:40:07 -0500

    modified:   JS/admin-update.js 	modified:   JS/admin.js 	modified:   JS/lagoinha.js 	modified:   app.js

--- JS/lagoinha.js
+++ JS/lagoinha.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../HTML/lagoinha.html'));
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../HTML/admin.html'), function(err) {
+      if (err) {
+        next(err);
+      }
+    });
 });
+  
 module.exports = router;
