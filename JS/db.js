eedeb33677d968e277f413efa855f8f99ee45d09
DahVincis <ph.leao2099@gmail.com>
2023-11-21 13:39:28 -0500

    new file:   HTML/login.html 	modified:   JS/admin.js 	modified:   JS/db.js 	new file:   MODELS/user.js 	modified:   app.js

--- JS/db.js
+++ JS/db.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useUnifiedTopology: true});
+// Importing required modules
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+// Use sessions for tracking logins
+app.use(session({
+  secret: 'Lcc2023*', // Replace with your own secret key
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db // Your mongoose connection object
+  })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate())); // User is your user model
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
 // Import routes
 // These are the routes that handle the requests to the server
 const lagoRoutes = require('./JS/lagoinha.js');
