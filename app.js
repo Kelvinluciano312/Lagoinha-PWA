44ced74e2aa3ba0ecf42ddffec23bf68858b1a41
DahVincis <ph.leao2099@gmail.com>
2023-11-21 14:20:19 -0500

    modified:   HTML/login.html 	modified:   app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 // Set up express-session middleware
 app.use(session({
-  secret: 'your-secret-key', // Replace with your own secret key
+  secret: 'Lcc2023!', // Replace with your own secret key
   resave: false,
   saveUninitialized: true
 }));
