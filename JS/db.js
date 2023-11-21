714369a68dc8ac16ddf179b7a905417cd6882822
DahVincis <ph.leao2099@gmail.com>
2023-11-21 15:09:08 -0500

    modified:   JS/db.js

--- JS/db.js
+++ JS/db.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/', {useNewUrlParser: true, useUnifiedTopology: true});
-
 // Importing required modules
+const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/login', {useNewUrlParser: true, useUnifiedTopology: true});
+
 // Use sessions for tracking logins
 app.use(session({
-  secret: 'Lcc2023!', 
+  secret: 'Lcc2023*', 
   resave: true,
   saveUninitialized: false,
   store: new MongoStore({
