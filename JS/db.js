2bf6a15cd7aa07e45199b78291c1f1b4846747ef
DahVincis <ph.leao2099@gmail.com>
2023-11-22 11:55:49 -0500

    modified:   JS/db.js 	modified:   app.js

--- JS/db.js
+++ JS/db.js
@@ -1,26 +1,28 @@
-// Importing required modules
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+module.exports = function(app) {
+  // Importing required modules
+  const mongoose = require('mongoose');
+  const passport = require('passport');
+  const LocalStrategy = require('passport-local').Strategy;
+  const session = require('express-session');
+  const MongoStore = require('connect-mongo')(session);
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/login', {useNewUrlParser: true, useUnifiedTopology: true});
+  // Connect to MongoDB
+  mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/LagoinhaCT?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
-// Use sessions for tracking logins
-app.use(session({
-  secret: 'Lcc2023*', 
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection // Use mongoose.connection here
-  })
-}));
+  // Use sessions for tracking logins
+  app.use(session({
+    secret: 'Lcc2023*', 
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection // Use mongoose.connection here
+    })
+  }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-passport.use(new LocalStrategy(User.authenticate())); // User is your user model
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+  passport.use(new LocalStrategy(User.authenticate())); // User is your user model
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+}
