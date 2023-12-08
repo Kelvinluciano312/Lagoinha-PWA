73176f38ae335e7e9b4f115df33f937287c797c8
dxciphr <gasparmarij1@southernct.edu>
2023-12-08 12:25:59 -0500

    more fixes

--- JS/lagoinha-menu.js
+++ JS/lagoinha-menu.js
@@ -3,7 +3,7 @@ function openNav() {
   // Set the width of the side navigation to 250px
   document.getElementById("mySidenav").style.width = "250px";
   // Shift the page content to the right by 250px
-  document.getElementById("main").style.marginLeft = "250px";
+
   // Add a black background color to the body
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
@@ -13,7 +13,7 @@ function closeNav() {
   // Set the width of the side navigation to 0
   document.getElementById("mySidenav").style.width = "0";
   // Shift the page content to the left by 0px
-  document.getElementById("main").style.marginLeft = "0";
+
   // Set the background color of the body to white
   document.body.style.backgroundColor = "white";
 }
